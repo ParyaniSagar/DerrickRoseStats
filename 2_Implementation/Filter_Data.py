434df8de9390b6a5089870336f64b40059730ffa
Sagar Paryani <paryani7sagar@gmail.com>
2022-09-12 22:05:34 +0530

    Updates
    Major updates
    combined all data
    filtered data

--- 2_Implementation/Filter_Data.py
+++ 2_Implementation/Filter_Data.py
@@ -3,11 +3,38 @@ derrick_rose_tenure = list(range(2009,2023))
 
 for year in derrick_rose_tenure:
     raw_df = pd.read_csv("2_Implementation/2_Season_DF/Season_{}.csv".format(year))
+
     raw_df.drop(raw_df.index[raw_df['Rk'] == 'Rk'], inplace=True) #To remove the unnecessary headers in the table
+
+    # Remove instances of out of order values to make data more general
+    def normalise_row(row):
+        if row['MP'] == 'Did Not Play':
+            return "No"
+        if row['MP'] == 'Inactive':
+            return "No"
+        if row['MP'] == 'Did Not Dress':
+            return "No"
+        if row['MP'] == 'Not With Team':
+            return "No"
+        else:
+            return "Yes"
+    raw_df['Game Played'] = raw_df.apply(lambda row : normalise_row(row), axis=1) 
+    
+    raw_df.loc[raw_df["MP"] == "Did Not Play","MP"]= "00:00" # Replace with time related Minutes played
+    raw_df.loc[raw_df["MP"] == "Inactive","MP"] = "00:00"
+    raw_df.loc[raw_df["MP"] == "Did Not Dress","MP"] = "00:00"
+    raw_df.loc[raw_df["MP"] == "Not With Team","MP"] = "00:00"
+    
+
     raw_df[['Match Result','Points']] = raw_df['Unnamed: 7'].str.split('(', n=1, expand=True) # To split the match results and points in the columnm
     raw_df["Points"] = raw_df["Points"].str.strip(")")
-    raw_df.head(5)
-    raw_df.drop(['Unnamed: 0','Rk','G','Age','Unnamed: 5','Unnamed: 7'],axis=1,inplace=True) # Drop all unnecessry columns
     
-    raw_df.to_csv("2_Implementation/2_Season_DF/Season_{}.csv".format(year))
+    raw_df[["Minutes Played","Seconds Played"]] = raw_df["MP"].str.split(":",expand=True)
+    
+
+    raw_df.drop(['Unnamed: 0','Rk','G','Age','Unnamed: 5','Unnamed: 7','MP'],axis=1,inplace=True) # Drop all unnecessry columns
+
+    
+
+    raw_df.to_csv("2_Implementation/2_Season_DF/Filtered_Season_{}.csv".format(year))
 
--- README.md
+++ README.md
@@ -1,9 +1,20 @@
-# Derrick Rose Statistics
- 
+# Derrick Rose Career Stats
+![Derrick Rose](3_Material/DerrickRose.jpg)
 
- Requirements
- 1. when did Derrick Rose enter the NBA Season? - 2008
+## Requirements
+ 1. When did Derrick Rose enter the NBA Season? - 2008
  2. On what occasions did the team qualify for the playoffs - And what was derrick rose's performance in the regular season!
 
 # NBA FORMAT
 ![NBA_FORMAT](3_Material/NBA_Flowchart.png)
+
+Tableau
+https://public.tableau.com/app/profile/sagar.paryani/viz/DerrickRoseCareerStats/winLosschart?publish=yes
+
+## Insights
+
+1. Average points scored by DR in games
+2. What is his Free throw, Field Goal, 3 pointer percentage
+3. Is he a foul  player? - fouls
+4. 
+
